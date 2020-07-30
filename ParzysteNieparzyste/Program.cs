3c19ab0d1a1074da08628d5f17ae42bbf29accd0
Kacper Barański <kacper.baranski@hotmail.com>
2020-07-31 00:03:39 +0200

    FIRST CHANGE

---
+++ ParzysteNieparzyste/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParzysteNieparzyste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Podaj liczbe:");
+                if (Liczba() % 2 == 0)
+                {
+                    Console.WriteLine("To liczba parzysta");
+                }
+                else Console.WriteLine("To liczba nieparzysta");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                //Waiting for something 
+                Console.ReadKey();
+            }
+
+        }
+
+        private static int Liczba()
+        {
+            if (!int.TryParse(Console.ReadLine(), out int value)) throw new Exception("Podana Wartosć jest nie prawidłowa!");
+            return value;
+        }
+    }
+}
